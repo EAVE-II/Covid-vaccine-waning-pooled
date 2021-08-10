59b8c5010bb26ad08fe2198b660ec1db5fd5d858
MulhollandRH <rachel.mulholland@ed.ac.uk>
2021-08-10 14:11:24 +0100

    Update for subgroups age and sex

--- code/07_meta_analysis.R
+++ code/07_meta_analysis.R
@@ -10,6 +10,8 @@
 
 #### 1 - Table of daily follow-up #####
 
+z.yr <- tcut(rep(0,nrow(df_cc_ps_matches)), c(-1,seq(0,max(df_cc_ps_matches$time_to_hosp),by=1) ))
+
 ## All vaccines
 z.agg <- pyears(Surv(time_to_hosp,event) ~ vacc + z.yr,
                 data=df_cc_ps_matches , scale=1, data.frame=TRUE)
@@ -46,3 +48,61 @@ daily_gam_output <- bind_rows(daily_gam_all, daily_gam_vacc) %>%
 
 write.csv(daily_gam_output, paste0("./output/meta-analysis/tbl_gam_", z_event_endpoint, ".csv"),
           row.names = F)
+
+
+
+##### 2 - Table of daily follow-up by Age and Sex #####
+
+## Link age and sex into data
+# Group age into 2 groups to allow for sufficient sample sizes
+df_cc_ps_matches <- df_cc_ps_matches %>%
+  mutate(age_grp2 = ifelse(age_grp == "18-64", "18-64", "65+")) %>%
+  left_join(select(df_cohort, EAVE_LINKNO, Sex))
+
+
+# Function (if helpful)
+GAM_tbl_var <- function(variable){
+  
+  # Day
+  z.yr <- tcut(rep(0,nrow(df_cc_ps_matches)), c(-1,seq(0,max(df_cc_ps_matches$time_to_hosp),by=1) ))
+  
+  
+  # Formula
+  z.fmla <- as.formula(paste("Surv(time_to_hosp,event)",
+                             " ~ vacc + z.yr + vacc_type +",
+                             paste(variable, collapse= "+")))
+  
+  
+  z.agg <- pyears(z.fmla,
+                  data=df_cc_ps_matches , scale=1, data.frame=TRUE)
+  
+  
+  daily_gam_all <- z.agg$data %>%
+    mutate(z.yr =  as.numeric(z.yr)-1) %>%
+    filter(z.yr <= 90)
+  
+  daily_gam_all <- daily_gam_all %>%
+    select(-pyears) %>%
+    pivot_wider(names_from = vacc, values_from = c("n", "event")) %>%
+    select(vacc_type, !!sym(variable), z.yr, n_uv, event_uv, n_vacc, event_vacc) 
+  
+  daily_gam_all
+
+}
+
+# Sex
+daily_gam_output_sex <- GAM_tbl_var("Sex") %>%
+  rename(Subgroup = Sex)
+
+# Age (Binary age)
+daily_gam_output_age <- GAM_tbl_var("age_grp2")%>%
+  rename(Subgroup = age_grp2)
+
+# Stack ontop
+daily_gam_output_agesex <- bind_rows(daily_gam_output_sex, daily_gam_output_age)
+
+# Output
+write.csv(daily_gam_output_agesex, paste0("./output/meta-analysis/tbl_gam_agesex_", z_event_endpoint, ".csv"),
+          row.names = F)
+
+
